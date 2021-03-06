4d333ea24fe86261d5b3baabc9722b57057a1785
Sarah Mounho <sarahmounho@gmail.com>
2021-03-05 18:00:46 -0600

    Made changes - working for now

--- SF06/totalizer/totalizer.ino
+++ SF06/totalizer/totalizer.ino
@@ -35,27 +35,40 @@
 #include <Wire.h> // Arduino library for I2C
 
 const int ADDRESS = 0x08; // Standard address for LD20 Liquid Flow Sensors
-const float SCALE_FACTOR_FLOW = 500.0;
+const float SCALE_FACTOR_FLOW = 10.0;
 const float SCALE_FACTOR_TEMP = 200.0;
-const char *UNIT_FLOW = " ml/min";
+const char *UNIT_FLOW = " ul/min";
 const char *UNIT_TEMP = " deg C";
 
+int ret;
 int16_t signed_flow_value;
-
 float sensor_reading;
 double totalizer_volume = 0.0;
-uint16_t counter = 0;
+int16_t counter = -1;
 double delta_t;
-  
 double this_micros;
 double last_micros;
 
+// -----------------------------------------------------------------------------
+// Measurement routine
+// -----------------------------------------------------------------------------
+void measure_flow(){
+  Serial.println("Begin Measure Flow");
+  delay(10);
+  Wire.requestFrom(ADDRESS, 3);
+  if (Wire.available() < 3) {
+    Serial.println("Error while reading flow measurement");
+  }
 
+  signed_flow_value  = Wire.read() << 8; // read the MSB from the sensor
+  signed_flow_value |= Wire.read();      // read the LSB from the sensor
+
+  sensor_reading = ((float) signed_flow_value) / SCALE_FACTOR_FLOW;
+}
 // -----------------------------------------------------------------------------
 // Arduino setup routine, just runs once:
 // -----------------------------------------------------------------------------
 void setup() {
-  int ret;
 
   Serial.begin(9600); // initialize serial communication
   Wire.begin();       // join i2c bus (address optional for master)
@@ -70,40 +83,45 @@ void setup() {
       delay(500); // wait long enough for chip reset to complete
     }
   } while (ret != 0);
-
   delay(50); // wait long enough for chip reset to complete
-}
 
-void measure_flow(){
-  Wire.requestFrom(ADDRESS, 3);
-  if (Wire.available() < 3) {
-    Serial.println("Error while reading flow measurement");
+  // Begin measurement
+  Wire.beginTransmission(ADDRESS);
+  Wire.write(0x36);
+  Wire.write(0x08);
+  ret = Wire.endTransmission();
+  if (ret != 0) {
+    Serial.println("Error while sending start measurement command, retrying...");
   }
-
-  signed_flow_value  = Wire.read() << 8; // read the MSB from the sensor
-  signed_flow_value |= Wire.read();      // read the LSB from the sensor
-
-  sensor_reading = ((float) signed_flow_value) / SCALE_FACTOR_FLOW;
 }
+
+
 // -----------------------------------------------------------------------------
 // The Arduino loop routine runs over and over again forever:
 // -----------------------------------------------------------------------------
 void loop(){
+  
+  
+
   // init
   this_micros = micros();
   totalizer_volume = 0.0;
 
   while (true) {
     counter++;
-    // check volume every 1 ms
-    if (counter % 0 == 0){
-      measure_flow();
-      last_micros = this_micros;
-      this_micros = micros();
-      delta_t = ((this_micros - last_micros)) / 1000000.0 / 60.0 ; // ml/min
-      totalizer_volume = totalizer_volume + sensor_reading * delta_t;
-      Serial.print("Totalizer (ml): "); Serial.println(totalizer_volume);
-    }
-    delay(5); // millisecond delay between rounds
+    measure_flow();
+    last_micros = this_micros;
+    this_micros = micros(); // get microseconds since board has been running program
+    delta_t = ((this_micros - last_micros)) / 1000000.0 / 60.0 ; // ul/min
+    totalizer_volume = totalizer_volume + sensor_reading * delta_t ; // seems to be off by 0.1 ul/min
+    Serial.print("Reading (ul): ");
+    Serial.print(sensor_reading);
+    Serial.print("Delta_t (min passed): ");
+    Serial.print(delta_t);
+    Serial.print("Totalizer (ul): "); 
+    Serial.println(totalizer_volume);
+    delay(20); // 
+    
+
   }
 }
